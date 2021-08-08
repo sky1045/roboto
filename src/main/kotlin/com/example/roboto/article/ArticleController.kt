48832e590828b4c64dfa4ec0c70c375eb4a36040
Robert <ghdwlgud4545@gmail.com>
2021-08-08 23:58:53 +0900

    cors to global config

--- src/main/kotlin/com/example/roboto/article/ArticleController.kt
+++ src/main/kotlin/com/example/roboto/article/ArticleController.kt
@@ -1,10 +1,7 @@
 package com.example.roboto.article
 
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import java.util.*
 
 @RestController
